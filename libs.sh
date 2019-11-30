33266d5610ae4362a998f5f8ed9785acfeacad06
Walkley He <walkleyhe@gmail.com>
2019-11-30 14:58:41 +0800

    Add Helm v3

--- libs.sh
+++ libs.sh
@@ -1,5 +1,11 @@
 export KUBECONFIG=/tmp/kubeconfig
 
+HELM_HOME=/tmp/helm
+mkdir -p $HELM_HOME
+export XDG_CACHE_HOME=$HELM_HOME/.cache
+export XDG_CONFIG_HOME=$HELM_HOME/.config
+export XDG_DATA_HOME=$HELM_HOME/.data
+
 update_kubeconfig(){
     aws eks update-kubeconfig --name "$1"  --kubeconfig /tmp/kubeconfig
 }
