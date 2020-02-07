d2f83b49f9a83d2a7c42f602b1e5660bee45a95b
Moritz Graf <moritz.graf@continental-corporation.com>
2020-02-07 15:00:40 +0100

    Removing typo in proxy variable

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 
 TAG='aws-lambda-layer-kubectl'
 
-docker build --build-arg HTTP_PROXYX --build-arg HTTPS_PROXY --build-arg http_proxy --build-arg https_proxy --no-cache -t $TAG .
+docker build --build-arg HTTP_PROXY --build-arg HTTPS_PROXY --build-arg http_proxy --build-arg https_proxy --no-cache -t $TAG .
 CONTAINER=$(docker run -d $TAG false)
 docker cp ${CONTAINER}:/layer.zip layer.zip
