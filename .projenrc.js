f79c68e293c8271cdb61b43418994b2638dfd51f
Pahud Hsieh <pahudnet@gmail.com>
2021-09-04 02:19:51 +0000

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,14 @@
+const { AwsCdkTypeScriptApp } = require('projen');
+const project = new AwsCdkTypeScriptApp({
+  cdkVersion: '1.95.2',
+  defaultReleaseBranch: 'main',
+  name: 'aws-lambda-layer-kubectl',
+
+  // cdkDependencies: undefined,  /* Which AWS CDK modules (those that start with "@aws-cdk/") this app uses. */
+  // deps: [],                    /* Runtime dependencies of this module. */
+  // description: undefined,      /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],                 /* Build dependencies for this module. */
+  // packageName: undefined,      /* The "name" in package.json. */
+  // release: undefined,          /* Add release management to this project. */
+});
+project.synth();
