984a14c6b2013f11092fca38ca6bbc5634daedd6
Pahud Hsieh <pahudnet@gmail.com>
2021-09-17 15:53:02 +0800

    bump versions (#58)

--- .projenrc.js
+++ .projenrc.js
@@ -35,9 +35,9 @@ const common_exclude = [
   'coverage',
 ];
 
-
 project.package.addField('resolutions', {
-  'pac-resolver': '5.0.0',
+  'pac-resolver': '^5.0.0',
+  'set-value': '^4.0.1',
 });
 
 project.npmignore.exclude(...common_exclude);
